5591252542d7d5f6afd999d156b2cb2d4ab128e5
vivukhue <dangpkh@gmail.com>
2017-07-17 15:43:34 +0300

    Nhapten

---
+++ Nhapten/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	char ten[50];
+	printf("Nhap ten");
+	gets(ten);
+	printf("Xinchao %s",ten);
+}
